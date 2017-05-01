a971686d469e90f47d9abb75aeb1a8eba7f084f6
amigo00678 <amigo00678@gmail.com>
2017-05-01 17:55:47 +0400

    Update context.py
    A simple fix for an issue mentioned here:
    https://code.djangoproject.com/ticket/27722
    https://github.com/translate/pootle/issues/5442
    https://github.com/django-crispy-forms/django-crispy-forms/issues/553
    Can reproduce this for python 3.5 and django 1.11. Using django-bootstrap-breadcrumbs.
    http://django-bootstrap-breadcrumbs.readthedocs.io/en/latest/

--- django/template/context.py
+++ django/template/context.py
@@ -270,6 +270,8 @@ def make_context(context, request=None, **kwargs):
     """
     Create a suitable Context from a plain dict and optionally an HttpRequest.
     """
+    if context is not None and context.__class__.__name__ == 'RequestContext':
+        return context
     if context is not None and not isinstance(context, dict):
         raise TypeError('context must be a dict rather than %s.' % context.__class__.__name__)
     if request is None:
